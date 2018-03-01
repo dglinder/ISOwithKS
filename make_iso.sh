74924def52e82b4c165c4a47bbaf281f91484e3e
Linder, Daniel G <dglinder@west.com>
2018-03-01 14:25:01 -0500

    Almost working build process.

--- make_iso.sh
+++ make_iso.sh
@@ -18,24 +18,12 @@ set -e
 # Global variables to tweak.
 #
 # Location to build IOS in
-BUILDROOT=/home/dan/tmp/
-# Menu entry for the custom image menu entry
-# TODO - Get the new menu entry PER ISO (RHEL-7.2 in the LABEL), or dynamically generate it from the first menuentry.
-# For RedHat 7.2 ISO
-NEWMENU="menuentry Custom_Kickstart_Installation --class fedora --class gnu-linux --class gnu --class os {
-        linuxefi /images/pxeboot/vmlinuz inst.stage2=hd:LABEL=RHEL-7.2\x20Server.x86_64 quiet inst.ks=cdrom:/ks.cfg
-        initrdefi /images/pxeboot/initrd.img
-}"
-# For Centos ISO
-NEWMENU="menuentry Custom_Kickstart_Installation --class fedora --class gnu-linux --class gnu --class os {
-	linuxefi /images/pxeboot/vmlinuz inst.stage2=hd:LABEL=CentOS\x207\x20x86_64 quiet inst.ks=cdrom:/ks.cfg
-	initrdefi /images/pxeboot/initrd.img
-}"
-#
+BUILDROOT=/opt/rh/tmp/
+# Source of the RHEL ISOs - this can be a directory with symlinks.
+GOLDENISO=./isos/
+
 # Timestamp for the image:
 TS=$(date +%Y%m%d_%H%M%S)
-#
-#################################
 
 #################################
 # Ensure root permissions
@@ -50,45 +38,63 @@ fi
 #
 function clean_exit {
   echo "Exiting using exit function."
-  umount ${BUILDDIR}/isomount/
-#  rmdir ${BUILDDIR}/isomount/
-#  rmdir ${BUILDDIR}/
+  if [ ! -z "${BUILDDIR}" ] ; then
+    umount ${BUILDDIR}/isomount/
+#    rmdir ${BUILDDIR}/isomount/
+#    rmdir ${BUILDDIR}/
+  fi
 }
 trap clean_exit EXIT
 
 #################################
 # Show user the possible ISO files to use
+if [ $(ls -1 ${GOLDENISO}/ | wc -l) -le 0 ] ; then
+  echo "No files found in the ISO source directory, exiting."
+  exit 1
+fi
+
 echo "List of available ISOs"
-ls -1 ./golden_isos/ | cat -n
+ls -1 ${GOLDENISO}/ | egrep -i iso$ | cat -n
 ISONAME="bogus"
 if [ ! -z "$1" ] ; then
   ISONAME="$1"
   echo "Using command line ISO: ${ISONAME}"
 fi
 
-KSNAME="bogus"
-if [ ! -z "$2" ] ; then
-  KSNAME="$2"
-  echo "Using command line kickstart: ${KSNAME}"
-fi
-
-while [ ! -e "./golden_isos/${ISONAME}" ] ; do
+while [ ! -e "${GOLDENISO}/${ISONAME}" ] ; do
   read -p "Please enter the full name of the ISO to use: " -i "${ISONAME}" ISONAME
-  if [ ! -e "./golden_isos/${ISONAME}" ] ; then
+  if [ ! -e "${GOLDENISO}/${ISONAME}" ] ; then
     echo "Invalid ISO name.  Try again."
   fi
 done
 
-#BUILDDIR=$(mktemp -d ${ISONAME}_tempdir_XXXX --tmpdir=${BUILDROOT})
+ISONAME=$(basename "${ISONAME}" .iso)
+
+if [ ! -e ./media_info/${ISONAME}.info.sh ] ; then
+  echo "Missing media info file: ./media_info/${ISONAME}.info.sh "
+  exit 1
+fi
+
+source "./media_info/${ISONAME}.info.sh"
+
+if [ -z "${KSNAME}" ] ; then
+  echo "Missing name of kickstart file to use."
+  exit 1
+fi
+
+# Setup the location to hold temporary files and build the ISO.
 BUILDDIR="${BUILDROOT}/${ISONAME}_tempdir"
 
+# Ensure temporary directories exist.
 mkdir -p ${BUILDDIR}/
 mkdir -p ${BUILDDIR}/isomount/
 mkdir -p ${BUILDDIR}/image/
-mount -o loop ./golden_isos/${ISONAME} ${BUILDDIR}/isomount/
+
+# Mount the ISO
+mount -o loop ${GOLDENISO}/${ISONAME}.iso ${BUILDDIR}/isomount/
 
 # Cleanup from past tests
-rm -f ${BUILDDIR}/image/isolinux/isolinux.cfg* ${BUILDDIR}/image/ks.cfg* ${BUILDDIR}/image/EFI/BOOT/grub.cfg*
+rm -f ${BUILDDIR}/${MBR_MENUFILE}* ${BUILDDIR}/image/ks.cfg* ${BUILDDIR}/${UEFI_MENUFILE}*
 
 # Copy contents of ISO to new working directory
 echo "Copying contents from ISO to ${BUILDDIR}/image/"
@@ -97,29 +103,43 @@ rsync -a ${BUILDDIR}/isomount/ ${BUILDDIR}/image/
 # Copy the kickstart file into the ISO directory
 rsync -a ./ksfiles/${KSNAME} ${BUILDDIR}/image/ks.cfg
 
-ls -altr ${BUILDDIR}/image/isolinux/isolinux.cfg* ${BUILDDIR}/image/ks.cfg* ${BUILDDIR}/image/EFI/BOOT/grub.cfg*
-echo Pausing after copy
-read -p "Pausing after copy" foo
+#ls -altr ${BUILDDIR}/${MBR_MENUFILE}* ${BUILDDIR}/image/ks.cfg* ${BUILDDIR}/${UEFI_MENUFILE}*
+#read -p "Pausing after copy, press return to continue." foo
 
-#################################
-# Setup the EFI portion of the boot files (modern systems)
-#
-# Edit the grub.conf in the ../EFI/BOOT/ directory:
-# 1: Change the menuentry title of the first entry:
-sed -i.$(date +%s) '0,/menuentry /s//ABCDEFGHIJ\nmenuentry /' ${BUILDDIR}/image/EFI/BOOT/grub.cfg
+#set -x
+if [ ${DO_UEFI} -ge 1 ] ; then
+  #################################
+  # Setup the EFI portion of the boot files (modern systems)
+  #
+  # Edit the grub.conf in the ../EFI/BOOT/ directory:
+  # 1: Change the menuentry title of the first entry:
+  echo "===== Setting up UEFI boot options ====="
+  sed -i.$(date +%s) "0,/${UEFI_FIND}/s//ABCDEFGHIJ\n${UEFI_FIND}/" ${BUILDDIR}/image/EFI/BOOT/grub.cfg
+  sleep 1
+  
+  # 2: Insert new text:
+  INJECT=$(echo "${UEFI_MENU}" | sed ':a;N;$!ba;s/\n/\\n/g')
+  sed -i.$(date +%s) "s#ABCDEFGHIJ#${INJECT}#" ${BUILDDIR}/image/EFI/BOOT/grub.cfg
+  sleep 1
+  
+  # 3: Set default boot to the first one we setup:
+  sed -i.$(date +%s) 's/^set default=.../set default="0"/' ${BUILDDIR}/image/EFI/BOOT/grub.cfg
+  sleep 1
+  echo "Updated: ${BUILDDIR}/image/EFI/BOOT/grub.cfg"
+#  read -p "Pausing after UEFI settings, press return to continue." foo
+fi
 
-# 2: Insert new text:
-INJECT=$(echo "${NEWMENU}" | sed ':a;N;$!ba;s/\n/\\n/g')
-sed -i.$(date +%s) "s#ABCDEFGHIJ#${INJECT}#" ${BUILDDIR}/image/EFI/BOOT/grub.cfg
-
-# 3: Set default boot to the first one we setup:
-sed -i.$(date +%s) 's/^set default=.../set default="0"/' ${BUILDDIR}/image/EFI/BOOT/grub.cfg
-
-#################################
-# Setup the legacy portion of the boot files
-#
-# 1: Edit the isolinux.cft to use the new kickstart file.
-sed -i.$(date +%s) '0,/append initrd=initrd.img/s//append initrd=initrd.img  ks=cdrom:\/ks.cfg/' ${BUILDDIR}/image/isolinux/isolinux.cfg
+if [ ${DO_MBR} -ge 1 ] ; then
+  #################################
+  # Setup the legacy portion of the boot files
+  #
+  # 1: Edit the isolinux.cft to use the new kickstart file.
+  echo "===== Setting up legacy/MBR boot options ====="
+  sed -i.$(date +%s) "0,/${MBR_FIND}/s//${MBR_MENU}/" ${BUILDDIR}/${MBR_MENUFILE}
+  sleep 1
+  echo "Updated: ${BUILDDIR}/${MBR_MENUFILE}"
+#  read -p "Pausing after MBR settings, press return to continue." foo
+fi
 
 #################################
 # Now build the ISO image
@@ -138,13 +158,21 @@ sed -i.$(date +%s) '0,/append initrd=initrd.img/s//append initrd=initrd.img  ks=
 # -no-emul-boot - Specifies  that  the boot image used to create El Torito bootable CDs is a "no emulation" image.
 # -b - Specifies  the  path  and filename of the boot image to be used when making an El Torito bootable CD
 
+echo "Executing the \"mkisofs\" command."
+# Notes: https://fedoraproject.org/wiki/User:Pjones/BootableCDsForBIOSAndUEFI
+# mkisofs -o ${BUILDDIR}/custom-${ISONAME}.iso -b isolinux/isolinux.bin \
+#         -J -input-charset UTF-8 -R -l -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
+#         -boot-info-table -eltorito-alt-boot -e images/efiboot.img \
+#         -no-emul-boot -graft-points -V "${CDLABEL}" \
+#         ${BUILDDIR}/image/ 2>&1 | egrep -v 'estimate finish|^Using\ .*for\ '
+mkisofs -U  -A "${CDLABEL}" -V "${CDLABEL}" -volset "${CDLABEL}" -J  -joliet-long -r -v -T \
+    -o ${BUILDDIR}/custom-${ISONAME}.iso -b isolinux/isolinux.bin -c isolinux/boot.cat \
+    -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot \
+    -e images/efiboot.img -no-emul-boot \
+    ${BUILDDIR}/image/ 2>&1 | egrep -v 'estimate finish|^Using\ .*for\ '
 
-mkisofs -o ${BUILDDIR}/custom-${ISONAME}.iso -b isolinux/isolinux.bin \
-        -J -R -l -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
-        -boot-info-table -eltorito-alt-boot -e images/efiboot.img \
-        -no-emul-boot -graft-points -V "CentOS 7 x86_64 " \
-        ${BUILDDIR}/image/ 2>&1 | egrep -v 'estimate finish'
-
+echo "Execution of \"mkisofs\" complete."
+echo ""
 echo "Built ISO available here:"
 echo "${BUILDDIR}/custom-${ISONAME}.iso"
 ls -al "${BUILDDIR}/custom-${ISONAME}.iso"
--- media_info/rhel-server-7.4-x86_64-dvd.info.sh
+++ media_info/rhel-server-7.4-x86_64-dvd.info.sh
@@ -3,24 +3,28 @@
 SHA256SUM=""
 
 KSNAME="RHEL7.ks"
-CDLABEL="RHEL-7.4 Server"
+CDLABEL="RHEL-7.4 Server.x86_64"
+CDLABEL_FIXED=$(echo ${CDLABEL} | sed 's/\ /\\\\x20/g')
 
 # If the ISO supports MBR booting, here is the menu entry to use.
 DO_MBR=1
 # If DO_MBR==1, MBR_FIND = String to look for to replace with MBR_MENU in MBR_MENUFILE
-MBR_MENUFILE=""
-MBR_FIND=""
-MBR_MENU=""
+MBR_MENUFILE="image/isolinux/isolinux.cfg"
+MBR_FIND="append initrd=initrd.img"
+MBR_MENU="append initrd=initrd.img inst.ks=cdrom:\/ks.cfg"
 
 # If the ISO supports UEFI booting, here is the menu entry to use.
 DO_UEFI=1
 # If DO_UEFI==1, UEFI_FIND = String to look for to replace with UEFI_MENU in UEFI_MENUFILE
-UEFI_MENUFILE=""
-UEFI_FIND=""
-UEFI_MENU="menuentry Custom_Kickstart_Installation --class fedora --class gnu-linux --class gnu --class os {
-        linuxefi /images/pxeboot/vmlinuz inst.stage2=hd:LABEL=RHEL-7.2\x20Server.x86_64 quiet inst.ks=cdrom:/ks.cfg
-        initrdefi /images/pxeboot/initrd.img
+UEFI_MENUFILE="image/EFI/BOOT/grub.cfg"
+UEFI_FIND="menuentry "
+UEFI_MENU="menuentry 'Custom Red Hat Enterprise Linux 7.4' --class fedora --class gnu-linux --class gnu --class os {
+      linuxefi /images/pxeboot/vmlinuz inst.stage2=hd:LABEL=${CDLABEL_FIXED} inst.ks=cdrom:/ks.cfg
+      initrdefi /images/pxeboot/initrd.img
 }"
 
 
+echo ==== DEBUG: CDLABEL : ${CDLABEL}
+echo ==== DEBUG: CDLABEL_FIXED : ${CDLABEL_FIXED}
+echo ==== DEBUG: UEFI_MENU: ${UEFI_MENU}
 
