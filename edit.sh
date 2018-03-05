7c1a90c8cbe59632804da5940ee05005fae3adc9
Linder, Daniel G <dglinder@west.com>
2018-03-05 17:47:24 -0500

    Sample file for multi-line edit/replace using awk

---
+++ edit.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -u
+set -e
+
+file="isolinux.cfg.GOLDEN"
+tempfile="isolinux.cfg.NEW"
+
+find_start="label linux"
+find_end="Server.x86_64 quiet"
+sub="label bootlocal
+  menu default
+  localboot 0x80
+
+label custom
+  menu label ^Install Custom Red Hat Enterprise Linux 7.4
+  kernel vmlinuz
+  append initrd=initrd.img inst.ks=cdrom:/ks.cfg biosdevname=0 net.ifnames=0 inst.stage2=hd:LABEL=RHEL-7.4\x20Server.x86_64
+"
+
+cp -f $file $tempfile
+awk -v sb="$sub" "/${find_start}/,/${find_end}/ { if ( \$0 ~ /${find_start}/ ) print sb; next } 1" "$file" > "$tempfile"
+
+diff -C2 $file $tempfile
+
+
