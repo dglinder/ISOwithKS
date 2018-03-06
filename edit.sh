d1f517a5f02536eec810b4837ff275b174045442
Linder, Daniel G <dglinder@west.com>
2018-03-06 13:11:37 -0500

    Testing file with awk configuration editing.

--- edit.sh
+++ edit.sh
@@ -6,7 +6,7 @@ file="isolinux.cfg.GOLDEN"
 tempfile="isolinux.cfg.NEW"
 
 find_start="label linux"
-find_end="Server.x86_64 quiet"
+find_end="append initrd.*Server"
 sub="label bootlocal
   menu default
   localboot 0x80
@@ -16,6 +16,15 @@ label custom
   kernel vmlinuz
   append initrd=initrd.img inst.ks=cdrom:/ks.cfg biosdevname=0 net.ifnames=0 inst.stage2=hd:LABEL=RHEL-7.4\x20Server.x86_64
 "
+
+echo "Looking for:"
+echo "    ${find_start}"
+echo "    ${find_end}"
+echo ""
+echo "Replace with:"
+echo "--begin--"
+echo "${sub}"
+echo "--end--"
 
 cp -f $file $tempfile
 awk -v sb="$sub" "/${find_start}/,/${find_end}/ { if ( \$0 ~ /${find_start}/ ) print sb; next } 1" "$file" > "$tempfile"
