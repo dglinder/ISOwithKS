46983be4af8e1850c1f954326648bac5c6ba5e77
Dan Blackpoole Linder <dan@linder.org>
2020-09-20 18:10:55 -0500

    Fix up the boot options for bootstrap node

--- media_info/fedora-coreos-32.20200824.3.0-live.x86_64.info.sh
+++ media_info/fedora-coreos-32.20200824.3.0-live.x86_64.info.sh
@@ -6,28 +6,24 @@ KSNAME="fedora-coreos-32.ks"
 CDLABEL="fedora-coreos-32.20200824.3.0"
 CDLABEL_FIXED=$(echo ${CDLABEL} | sed 's/\ /\\\\x20/g')
 
+BOOTSTRAP_NODE="coreos.inst.install_dev=/dev/sda coreos.inst.image_url=http://192.168.65.237:8080/okd4/fcos.raw.xz coreos.inst.ignition_url=http://192.168.65.237:8080/okd4/bootstrap.ign"
+CONTROLPLANE_NODE="coreos.inst.install_dev=/dev/sda coreos.inst.image_url=http://192.168.65.237:8080/okd4/fcos.raw.xz coreos.inst.ignition_url=http://192.168.65.237:8080/okd4/master.ign"
+COMPUTE_NODE="coreos.inst.install_dev=/dev/sda coreos.inst.image_url=http://192.168.65.237:8080/okd4/fcos.raw.xz coreos.inst.ignition_url=http://192.168.65.237:8080/okd4/worker.ign"
 #############################################
 # If the ISO supports MBR booting, here is the menu entry to use.
 #
 DO_MBR=1
-# If DO_MBR==1, MBR_FIND = String to look for to replace with MBR_MENU in MBR_MENUFILE
+# If DO_MBR==1, Replace tex in MBR_MENUFILE between MBR_START
+# and MBR_END with MBR_MENU
 MBR_MENUFILE="image/isolinux/isolinux.cfg"
 # NOTE: These match on a line, so the WHOLE LINE WILL BE CHANGED.
 MBR_START="^label linux"
-MBR_END="append initrd=initrd.img"
-MBR_MENU="label bootlocal
-  localboot 0x80
+MBR_END="^####"
+MBR_MENU="label linux
+  menu label ^Fedora CoreOS (Live) Customized for Bootstrap Node
   menu default
-
-label custom
-  menu label Install ^Custom Red Hat Enterprise Linux 7.4
-  kernel vmlinuz
-  append initrd=initrd.img inst.stage2=hd:LABEL=${CDLABEL_FIXED} inst.ks=cdrom:/ks.cfg biosdevname=0 net.ifnames=0
-
-label linux
-  menu label ^Install Red Hat Enterprise Linux 7.4
-  kernel vmlinuz
-  append initrd=initrd.img inst.stage2=hd:LABEL=RHEL-7.4\x20Server.x86_64 quiet
+  kernel /images/pxeboot/vmlinuz
+  append initrd=/images/pxeboot/initrd.img,/images/ignition.img mitigations=auto,nosmt systemd.unified_cgroup_hierarchy=0 coreos.liveiso=fedora-coreos-32.20200824.3.0 ignition.firstboot ignition.platform.id=metal ${BOOTSTRAP_NODE}
 "
 #
 #############################################
@@ -38,11 +34,10 @@ label linux
 DO_UEFI=1
 # If DO_UEFI==1, UEFI_FIND = String to look for to replace with UEFI_MENU in UEFI_MENUFILE
 UEFI_MENUFILE="image/EFI/fedora/grub.cfg"
-UEFI_START="^menuentry.*Install Red Hat Enterprise Linux"
-UEFI_END="^}"
-UEFI_MENU="menuentry 'Custom Red Hat Enterprise Linux 7.4' --class fedora --class gnu-linux --class gnu --class os {
-      linuxefi /images/pxeboot/vmlinuz inst.stage2=hd:LABEL=${CDLABEL_FIXED} inst.ks=cdrom:/ks.cfg biosdevname=0 net.ifnames=0
-      initrdefi /images/pxeboot/initrd.img
+UEFI_START="^menuentry.*Fedora CoreOS"
+UEFI_END="metal$"
+UEFI_MENU="menuentry 'Fedora CoreOS (Live) - Bootstrap Node' --class fedora --class gnu-linux --class gnu --class os {
+      linux /images/pxeboot/vmlinuz mitigations=auto,nosmt systemd.unified_cgroup_hierarchy=0 coreos.liveiso=fedora-coreos-32.20200824.3.0 ignition.firstboot ignition.platform.id=metal ${BOOTSTRAP_NODE}
 }
 "
 #
